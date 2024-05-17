044f411a041d039718728609642847294338a541
huangxiaofeng10047 <hxf168482@gmail.com>
2024-05-17 11:09:53 +0800

    chore: codesnap save path

--- nvim/.config/nvim/lua/plugins/codesnap.lua
+++ nvim/.config/nvim/lua/plugins/codesnap.lua
@@ -3,5 +3,7 @@ require("codesnap").setup({
   border = "rounded",
   has_breadcrumbs = true,
   bg_theme = "grape",
+  save_path = "~/Pictures",
+  parsed= "~/Pictures/CodeSnap_y-m-d_at_h:m:s.png",
   watermark = ""
 })
